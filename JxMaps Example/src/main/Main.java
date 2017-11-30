cb2bbfaa7e470c76da2b47490c371306129ae4f0
Gabri1495 <gabry@Obi-Wan95.station>
2017-11-30 12:27:16 +0100

    code refactoring: 	° refactored Main's code

--- JxMaps Example/src/main/Main.java
+++ JxMaps Example/src/main/Main.java
@@ -1,31 +1,12 @@
 package main;
 
 import gui.SimConfigurationFrame;
-import simulationConfiguration.SimulationConfig;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		//points = new ArrayList<LatLng>();
-		//MapExample map = new MapExample();
-		//Gui gui = new Gui(map);	
-		
 		SimConfigurationFrame configurator = new SimConfigurationFrame();
-		configurator.setVisible(true);
-		
-		//SimulationConfig.getInstance().setSimulationVelocity(2.25);
-		
-		//Aggregator aggregator = new Aggregator("BUS32", map);
-		//aggregator.start();
-		
-		//Aggregator aggregator2 = new Aggregator("BUS33", map);
-		//aggregator2.start();
-				
-		//new Bus("BUS32", "gpx/32.gpx").start();
-		//new Bus("BUS33", "gpx/path2.gpx").start();
-		
-
-		
+		configurator.setVisible(true);		
 	}
 
 }
