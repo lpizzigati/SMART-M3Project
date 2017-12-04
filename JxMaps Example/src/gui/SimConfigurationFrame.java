aa0ce088ee9823cb1277615992744db93d087592
nicioniz <nicio.fava@gmail.com>
2017-12-04 19:08:27 +0100

    Add simulation velocity slider.

--- JxMaps Example/src/gui/SimConfigurationFrame.java
+++ JxMaps Example/src/gui/SimConfigurationFrame.java
@@ -16,6 +16,8 @@ import main.BusVisualizerAggregator;
 import main.Bus;
 import main.BusMap;
 import simulationConfiguration.SimulationConfig;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
 
 @SuppressWarnings("serial")
 public class SimConfigurationFrame extends JFrame {
@@ -23,6 +25,8 @@ public class SimConfigurationFrame extends JFrame {
 	private JPanel contentPane;
 	private BusMap busMap;
 	private JCheckBox lineNo32CheckBox, lineNo20CheckBox;
+	private JLabel lblSimulationVelocity;
+	private JSlider velocitySlider;
 	
 	public SimConfigurationFrame() {
 		setTitle("SimConfiguration");
@@ -34,24 +38,23 @@ public class SimConfigurationFrame extends JFrame {
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		lineNo20CheckBox = new JCheckBox("Line No. 20");
-		lineNo20CheckBox.setSelected(true);
-		GridBagConstraints gbc_lineNo20CheckBox = new GridBagConstraints();
-		gbc_lineNo20CheckBox.insets = new Insets(0, 0, 5, 0);
-		gbc_lineNo20CheckBox.gridx = 0;
-		gbc_lineNo20CheckBox.gridy = 3;
-		contentPane.add(lineNo20CheckBox, gbc_lineNo20CheckBox);
+		lblSimulationVelocity = new JLabel("Simulation velocity");
+		GridBagConstraints gbc_lblSimulationVelocity = new GridBagConstraints();
+		gbc_lblSimulationVelocity.insets = new Insets(0, 0, 5, 0);
+		gbc_lblSimulationVelocity.gridx = 2;
+		gbc_lblSimulationVelocity.gridy = 2;
+		contentPane.add(lblSimulationVelocity, gbc_lblSimulationVelocity);
 		
 		lineNo32CheckBox = new JCheckBox("Line No. 32");
 		lineNo32CheckBox.setSelected(true);
 		GridBagConstraints gbc_lineNo32CheckBox = new GridBagConstraints();
-		gbc_lineNo32CheckBox.insets = new Insets(0, 0, 5, 0);
+		gbc_lineNo32CheckBox.insets = new Insets(0, 0, 5, 5);
 		gbc_lineNo32CheckBox.gridx = 0;
 		gbc_lineNo32CheckBox.gridy = 2;
 		contentPane.add(lineNo32CheckBox, gbc_lineNo32CheckBox);
@@ -59,14 +62,38 @@ public class SimConfigurationFrame extends JFrame {
 		JButton startSimButton = new JButton("START SIM");
 		startSimButton.addActionListener(this::startSimButtonPressed);
 		
+		lineNo20CheckBox = new JCheckBox("Line No. 20");
+		lineNo20CheckBox.setSelected(true);
+		GridBagConstraints gbc_lineNo20CheckBox = new GridBagConstraints();
+		gbc_lineNo20CheckBox.insets = new Insets(0, 0, 5, 5);
+		gbc_lineNo20CheckBox.gridx = 0;
+		gbc_lineNo20CheckBox.gridy = 3;
+		contentPane.add(lineNo20CheckBox, gbc_lineNo20CheckBox);
+		
+		velocitySlider = new JSlider();
+		velocitySlider.setToolTipText("Simulation velocity (from 0.1x to 2.0x)");
+		velocitySlider.setValue(15);
+		velocitySlider.setMinimum(1);
+		velocitySlider.setMaximum(20);
+		GridBagConstraints gbc_velocitySlider = new GridBagConstraints();
+		gbc_velocitySlider.insets = new Insets(0, 0, 5, 0);
+		gbc_velocitySlider.gridx = 2;
+		gbc_velocitySlider.gridy = 3;
+		contentPane.add(velocitySlider, gbc_velocitySlider);
+		
 		GridBagConstraints gbc_startSimButton = new GridBagConstraints();
+		gbc_startSimButton.insets = new Insets(0, 0, 0, 5);
 		gbc_startSimButton.gridx = 0;
-		gbc_startSimButton.gridy = 4;
+		gbc_startSimButton.gridy = 5;
 		contentPane.add(startSimButton, gbc_startSimButton);
 	}
 	
 	public void startSimButtonPressed(ActionEvent e) {
 		this.busMap = new BusMap();
+		
+		double simVel = velocitySlider.getValue() / 10.0;
+		SimulationConfig.getInstance().setSimulationVelocity(simVel);
+		
 		if(lineNo32CheckBox.isSelected()) {
 			BusVisualizerAggregator aggregator = new BusVisualizerAggregator("BUS32", busMap);
 			aggregator.start();
@@ -77,8 +104,7 @@ public class SimConfigurationFrame extends JFrame {
 			aggregator2.start();
 			new Bus("BUS20", "gpx/bus20.gpx").start();
 		}
-		new BusMapFrame(busMap);	
-		SimulationConfig.getInstance().setSimulationVelocity(1.5);
+		new BusMapFrame(busMap);		
 		this.dispose();
 	}
 }
